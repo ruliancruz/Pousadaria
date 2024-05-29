ca8e6d188d5333721cb46ea2c41e4b0ab1426bca
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-05-29 16:22:56 -0300

    develop inn booking test for groups registration

--- spec/system/inn_owner/inn_owner_register_group_booking_spec.rb
+++ spec/system/inn_owner/inn_owner_register_group_booking_spec.rb
@@ -2,6 +2,28 @@ require 'rails_helper'
 
 describe 'Inn Owner register booking for group' do
   it 'from the inn management page' do
-    
+    inn_owner = InnOwner
+                  .create! first_name: 'Joao',
+                    last_name: 'Almeida',
+                    document: '53783222001',
+                    email: 'joao@email.com',
+                    password: '123456'
+
+    inn = inn_owner
+            .create_inn! name: 'Pousada do Almeidinha',
+              registration_number: '30638898000199',
+              description: 'Um bom lugar',
+              address_attributes: {
+                address: 'Rua X',
+                number: '100',
+                city: 'Manaus',
+                state: 'AM',
+                postal_code: '69067-080',
+                neighborhood: 'Centro' }
+
+    login_as inn_owner, scope: inn_owner
+    visit inn_management_path
+
+    expect(page).to have_link 'Fazer Reserva Para Grupo'
   end
 end
